448ef161bd1aa4a10a74df389209207179df0ca1
budionomoto <budionomotoe3@gmail.com>
2018-11-25 19:04:04 +0700

    setelah penggunaan coroutine

--- app/src/main/java/id/xyzsystem/budiono/mission3jumat/detail/MatchDetailPresenter.kt
+++ app/src/main/java/id/xyzsystem/budiono/mission3jumat/detail/MatchDetailPresenter.kt
@@ -5,6 +5,9 @@ import id.xyzsystem.budiono.mission3jumat.api.ApiRepository
 import id.xyzsystem.budiono.mission3jumat.api.TheSportDBApi
 import id.xyzsystem.budiono.mission3jumat.model.MatchDetailResponse
 import id.xyzsystem.budiono.mission3jumat.model.TeamResponse
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
 import org.jetbrains.anko.doAsync
 import org.jetbrains.anko.uiThread
 
@@ -12,7 +15,8 @@ class MatchDetailPresenter(private val view: MatchDetailView,
                            private val apiRepository: ApiRepository,
                            private val gson: Gson
 ) {
-
+    /* sebelum penggunaan Coroutine */
+    /*
     fun getMatchDetail(evId: String) {
         view.showLoading()
         doAsync{
@@ -27,12 +31,27 @@ class MatchDetailPresenter(private val view: MatchDetailView,
             }
         }
     }
+    */
+    /* sesudah menggunakan coroutine*/
+    fun getMatchDetail(evId: String){
+        view.showLoading()
+        GlobalScope.launch(Dispatchers.Main) {
+            val data = gson.fromJson(apiRepository
+                .doRequest(TheSportDBApi.getMatchDetail(evId)).await(),
+                MatchDetailResponse::class.java
+            )
+            view.showMatchDetail(data.events)
+            view.hideLoading()
+        }
+    }
 }
 
 class TeamDetailPresenter(private val view: MatchDetailView,
                           private val apiRepository: ApiRepository,
                           private val gson: Gson
 ) {
+    /* sebelum penggunaan Coroutine */
+    /*
     fun getTeam(teamId: String, homeOrAway: Boolean){
         view.showLoading()
         doAsync {
@@ -45,6 +64,21 @@ class TeamDetailPresenter(private val view: MatchDetailView,
                 view.hideLoading()
                 view.showTeamDetail(datatim.teams, homeOrAway )
             }
+        }
+    }
+    */
+
+    /* sesudah penggunaan Coroutine */
+    fun getTeam(teamId: String, homeOrAway: Boolean){
+        view.showLoading()
+
+        GlobalScope.launch(Dispatchers.Main) {
+            val datatim = gson.fromJson(apiRepository
+                .doRequest(TheSportDBApi.getTeam(teamId)).await(),
+                TeamResponse::class.java
+            )
+            view.showTeamDetail(datatim.teams,homeOrAway)
+            view.hideLoading()
         }
     }
 }
