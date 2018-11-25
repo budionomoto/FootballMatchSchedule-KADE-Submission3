dd0ce45dd6c685d5e656122b2e949134a54f0006
budionomoto <budionomotoe3@gmail.com>
2018-11-25 19:09:31 +0700

    //penggunaan coroutine

--- app/src/main/java/id/xyzsystem/budiono/mission3jumat/match/MatchPresenter.kt
+++ app/src/main/java/id/xyzsystem/budiono/mission3jumat/match/MatchPresenter.kt
@@ -4,19 +4,27 @@ import com.google.gson.Gson
 import id.xyzsystem.budiono.mission3jumat.api.ApiRepository
 import id.xyzsystem.budiono.mission3jumat.api.TheSportDBApi
 import id.xyzsystem.budiono.mission3jumat.model.MatchResponse
-import org.jetbrains.anko.doAsync
-import org.jetbrains.anko.uiThread
+import id.xyzsystem.budiono.mission3jumat.utils.CoroutineContextProvider
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
+import okhttp3.Dispatcher
+import org.jetbrains.anko.coroutines.experimental.bg
 
 class MatchPresenter(private val view: MatchView,
                      private val apiRepository: ApiRepository,
-                     private val gson: Gson
+                     private val gson: Gson,
+                     private val context: CoroutineContextProvider = CoroutineContextProvider()
 ) {
 
-    fun getMatchList(eventID: String?,nextMatch:String?) {
+    fun getMatchList(leagueID: String?,nextMatch:String?) {
         view.showLoading()
+
+        /* sebelum coroutine */
+        /*
         doAsync {
             val data = gson.fromJson(apiRepository
-                .doRequest(TheSportDBApi.getMatch(eventID, nextMatch)),
+                .doRequest(TheSportDBApi.getMatch(leagueID, nextMatch)),
                 MatchResponse::class.java
             )
 
@@ -25,6 +33,18 @@ class MatchPresenter(private val view: MatchView,
                 view.showMatchList(data.events)
 
             }
+        }
+        */
+
+        /* sesudah penggunaan coroutine */
+        view.showLoading()
+        GlobalScope.launch(Dispatchers.Main){
+            val data = gson.fromJson(apiRepository
+                    .doRequest(TheSportDBApi.getMatch(leagueID,nextMatch)).await(),
+                    MatchResponse::class.java
+            )
+            view.showMatchList(data.events)
+            view.hideLoading()
         }
     }
 }
