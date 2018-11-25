ef71fd9e1626e0efa23f67c96dd6435c9fb68be8
budionomoto <budionomotoe3@gmail.com>
2018-11-25 19:08:58 +0700

    //automatic test instrument

---
+++ app/src/androidTest/java/id/xyzsystem/budiono/mission3jumat/MainActivityTest9.kt
@@ -0,0 +1,22 @@
+package id.xyzsystem.budiono.mission3jumat
+
+
+import android.support.test.filters.LargeTest
+import android.support.test.rule.ActivityTestRule
+import android.support.test.runner.AndroidJUnit4
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@LargeTest
+@RunWith(AndroidJUnit4::class)
+class MainActivityTest9 {
+
+    @Rule
+    @JvmField
+    var mActivityTestRule = ActivityTestRule(MainActivity::class.java)
+
+    @Test
+    fun mainActivityTest9() {
+    }
+}
