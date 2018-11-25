56940624dc7a361276ef725d5acabed087f8fe52
budionomoto <budionomotoe3@gmail.com>
2018-11-25 19:00:44 +0700

    //fun sesudah penggunaan coroutine

--- app/src/main/java/id/xyzsystem/budiono/mission3jumat/api/Repo.kt
+++ app/src/main/java/id/xyzsystem/budiono/mission3jumat/api/Repo.kt
@@ -1,9 +1,20 @@
 package id.xyzsystem.budiono.mission3jumat.api
 
+import kotlinx.coroutines.Deferred
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.async
 import java.net.URL
 
 class ApiRepository {
+    /* fun sebelum coroutine*/
+    /*
     fun doRequest(url: String): String {
         return URL(url).readText()
+    }
+    */
+
+    /* fun sesudah coroutine */
+    fun doRequest(url: String): Deferred<String> = GlobalScope.async {
+        URL(url).readText()
     }
 }
