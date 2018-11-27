e86e125d78a86bc5d894d4a986cdc332e172ecf6
budionomoto <budionomotoe3@gmail.com>
2018-11-27 10:30:29 +0700

    //Instrument test with behaviour

--- app/src/androidTest/java/id/xyzsystem/budiono/mission3jumat/BudionoInstrumentedTest.kt
+++ app/src/androidTest/java/id/xyzsystem/budiono/mission3jumat/BudionoInstrumentedTest.kt
@@ -2,14 +2,14 @@ package id.xyzsystem.budiono.mission3jumat
 
 import android.support.test.espresso.Espresso.onView
 import android.support.test.espresso.action.ViewActions.click
+import android.support.test.espresso.action.ViewActions.pressBack
 import android.support.test.espresso.assertion.ViewAssertions.matches
 import android.support.test.espresso.contrib.RecyclerViewActions
-import android.support.test.espresso.matcher.ViewMatchers.isDisplayed
-import android.support.test.espresso.matcher.ViewMatchers.withId
+import android.support.test.espresso.matcher.ViewMatchers.*
 import android.support.test.rule.ActivityTestRule
 import android.support.test.runner.AndroidJUnit4
 import android.support.v7.widget.RecyclerView
-import id.xyzsystem.budiono.mission3jumat.R.id.listEvent2
+import id.xyzsystem.budiono.mission3jumat.R.id.*
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
@@ -21,11 +21,48 @@ class MainActivityTest{
 
     @Test
     fun testRecylerViewBehaviour(){
+        /* memastikan recyclerView dengan id 'listEvent2' ditampilkan */
         onView(withId(listEvent2)).check(matches(isDisplayed()))
 
-        onView(withId(listEvent2)).perform(RecyclerViewActions.scrollToPosition<RecyclerView.ViewHolder>(1))
+        /* melakukan scroll pada recyclerview sampai dengan posisi ke-10*/
+        onView(withId(listEvent2)).perform(RecyclerViewActions.scrollToPosition<RecyclerView.ViewHolder>(10))
 
-        onView(withId(listEvent2)).perform(RecyclerViewActions.actionOnItemAtPosition<RecyclerView.ViewHolder>(1, click()))
+        /* klik item di posisi 10, untuk memastikan tampilkan detail*/
+        onView(withId(listEvent2)).perform(RecyclerViewActions.actionOnItemAtPosition<RecyclerView.ViewHolder>(10, click()))
+
+    }
+
+    @Test
+    fun TestAppBehaviour(){
+        /* memastikan bahwa terdapat sebuah spinner yang ditampilkan */
+        onView(withId(spinner2)).check(matches(isDisplayed()))
+
+        /* memberi tindakan klik pada spinner */
+        onView(withText("Spanish La Liga")).perform(click())
+
+        /* memberi tindakan klik pada sebuah teks yang ditentukan */
+        onView(withText("Barcelona")).check(matches(isDisplayed()))
+
+        /* memastikan bahwa suatu teks telah ditampilkan dan memberikan tindakan  klik pada teks tesebut */
+        onView(withText("Barcelona")).perform(click())
+
+        /* memastikan tombol favorite telah ditampilkan */
+        onView(withId(add_to_favorite_id)).check(matches(isDisplayed()))
+
+        /* memberi tindakan klik pada tombol favorite */
+        onView(withId(add_to_favorite_id)).perform(click())
+
+        /* memastikan teks 'Added to favorites' telah ditampilkan (snackbar)*/
+        onView(withText("Added to favorites")).check(matches(isDisplayed()))
+
+        /* menekan tombol kembali */
+        pressBack()
+
+        /* memastikan sebuah 'BottomNavigationView' telah ditampilkan */
+        onView(withId(bottom_navigation)).check(matches(isDisplayed()))
+
+        /* memberikan tindakan klik pada sebuah menu di 'BottomNavigationView' */
+        onView(withId(favoriteButtonId)).perform(click())
 
     }
 }
