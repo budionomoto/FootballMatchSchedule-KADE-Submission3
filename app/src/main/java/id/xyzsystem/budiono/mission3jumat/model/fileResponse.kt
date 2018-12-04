d705ecd90d0724afd6407b6f385d177cd9d381c1
budionomoto <budionomotoe3@gmail.com>
2018-12-04 14:19:35 +0700

    //no comment

--- app/src/main/java/id/xyzsystem/budiono/mission3jumat/model/fileResponse.kt
+++ app/src/main/java/id/xyzsystem/budiono/mission3jumat/model/fileResponse.kt
@@ -5,3 +5,5 @@ data class MatchResponse(val events: List<Match>)
 data class MatchDetailResponse(val events: List<MatchDetail>)
 
 data class TeamResponse(val teams: List<Team>)
+
+data class TeamsResponse(val teams: List<Teams>)
