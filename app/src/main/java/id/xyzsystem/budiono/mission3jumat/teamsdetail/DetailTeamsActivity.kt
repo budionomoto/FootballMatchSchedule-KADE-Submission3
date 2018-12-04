6205122162fe93c9ae148eb0a824256152349b93
budionomoto <budionomotoe3@gmail.com>
2018-12-04 14:18:58 +0700

    //no comment

---
+++ app/src/main/java/id/xyzsystem/budiono/mission3jumat/teamsdetail/DetailTeamsActivity.kt
@@ -0,0 +1,13 @@
+package id.xyzsystem.budiono.mission3jumat.teamsdetail
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import id.xyzsystem.budiono.mission3jumat.R
+
+class DetailTeamsActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_detail_teams)
+    }
+}
